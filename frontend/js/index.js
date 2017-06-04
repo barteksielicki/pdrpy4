d4ea672dc08b38a2b9f2968bef43eb3dbaf72ac9
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2017-06-04 16:48:39 +0200

    frontend: add form, update website look

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var API_URL = 'localhost:1337/'
+
 // base layer
 var base_layer = L.tileLayer(
   'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={access_token}',
@@ -49,3 +51,50 @@ var map = new CustomMap(
   [52.222122, 21.007040],
   13
 );
+
+
+var timepicker_options = {
+  twentyFour: true, //Display 24 hour format, defaults to false
+  upArrow: 'wickedpicker__controls__control-up', //The up arrow class selector to use, for custom CSS
+  downArrow: 'wickedpicker__controls__control-down', //The down arrow class selector to use, for custom CSS
+  close: 'wickedpicker__close', //The close class selector to use, for custom CSS
+  hoverState: 'hover-state', //The hover state class to use, for custom CSS
+  title: 'Timepicker', //The Wickedpicker's title,
+  showSeconds: false, //Whether or not to show seconds,
+  secondsInterval: 1, //Change interval for seconds, defaults to 1
+  minutesInterval: 1, //Change interval for minutes, defaults to 1
+  beforeShow: null, //A function to be called before the Wickedpicker is shown
+  show: null, //A function to be called when the Wickedpicker is shown
+  clearable: true, //Make the picker's input clearable (has clickable "x")
+};
+$('.timepicker').wickedpicker(timepicker_options);
+
+
+var update_speed = function(day, time, line) {
+  var query_params = {
+    'time': time,
+    'line': line
+  };
+  if (line)
+    query_params['line'] = line;
+  $.get(API_URL + 'speed', query_params)
+    .done(function(data) {
+      var records = data['records'];
+      for(var record in records) {
+        trams[record['line']].update(new_speed=record['speed']);
+      }
+    })
+    .fail(function() {
+      console.log('GET ERROR!');
+    });
+};
+
+$("#main-form").submit(function(event) {
+  event.preventDefault(); // to stop the form from submitting
+  update_speed(
+    $("select[name='day-select']")[0].value,
+    $("input[name='time-input']")[0].value,
+    $("input[name='line-input']")[0].value
+  );
+  this.submit();
+});
